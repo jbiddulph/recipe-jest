f543649a46427de1c53cd1ab95466cf13489875e
jbiddulph <john.mbiddulph@gmail.com>
2022-10-25 15:09:14 +0100

    preparing a base

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <WelcomeComponent msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import WelcomeComponent from "@/components/WelcomeComponent.vue";
 
 export default {
-  name: "HomeView",
+  name: "WelcomeComponent",
   components: {
-    HelloWorld,
+    WelcomeComponent,
   },
 };
 </script>
